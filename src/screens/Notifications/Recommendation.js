ef64010a7a55943c7f45c3f9ff8e6109a877f114
Navneeth Dhamotharan <navneethdhamotharan@gmail.com>
2024-11-24 19:07:27 -0800

    Added back eat time for ref + set const meet time

--- src/screens/Notifications/Recommendation.js
+++ src/screens/Notifications/Recommendation.js
@@ -318,8 +318,9 @@ const Recommendation = ({ route, navigation }) => {
               <View style={styles.row}>
                 <Ionicons name="time-outline" size={20} />
                 <NormalText paddingHorizontal={10} color="black">
-                    {route.params.event.startDate ? getTime(route.params.event.startDate.toDate()) : getTime(route.params.event.date.toDate())}
-                    {route.params.event.endDate && " - ".concat(getTime(route.params.event.endDate.toDate()))}
+                    {/* {route.params.event.startDate ? getTime(route.params.event.startDate.toDate()) : getTime(route.params.event.date.toDate())}
+                    {route.params.event.endDate && " - ".concat(getTime(route.params.event.endDate.toDate()))} */}
+                    6:00 pm - 7:00 pm
                 </NormalText>
               </View>
 
