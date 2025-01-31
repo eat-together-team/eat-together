d3e9e7dcd7c0166a6293a6c402d299dbee520f61
Eric Xiao <42785168+mathlord2@users.noreply.github.com>
2025-01-31 11:53:31 -0800

    Finished demo of Experiment.js

--- src/screens/Experiment.js
+++ src/screens/Experiment.js
@@ -17,12 +17,86 @@ import { db } from "../provider/Firebase";
 
 export default function ({ navigation }) {
     // State variables, functions
+    const [text, setText] = useState("");
+    const onChange = text => {
+        setText(text);
+    }
+
+    const pms = ["Megan Louie", "Helen Lan", "Valentina Filizola", "Ian Tsai", "Ananya Vaidyaraman", "Saara Uthmaan"];
+
+    const [longName, setLongName] = useState(false);
+    const [shortName, setShortName] = useState(false);
+    const [filteredPms, setFilteredPms] = useState([]);
+        
+    useEffect(() => {
+        let newPms = pms.filter((pm) => {
+            if (!longName && !shortName) {
+                return true;
+            }
+            if (longName && pm.length > 10) {
+                return true;
+            }
+            if (shortName && pm.length <= 10) {
+                return true;
+            }
+            return false;
+        });
+        setFilteredPms(newPms);
+    }, [longName, shortName]);
+
+    const [events, setEvents] = useState([]);
+    useEffect(() => {
+        db.collection("Public Events").onSnapshot((query) => {
+            let newEvents = [];
+            query.forEach((doc) => {
+                newEvents.push(doc.data().name);
+            });
+
+            setEvents(newEvents);
+        });
+    }, []);
+
+    const addUsername = username => {
+        if (username === "") {
+            return;
+        }
+        db.collection("WorkshopUsernames").doc(username).update({
+            newId: "workshop" + Date.now()
+        }).then(() => {
+            alert("Document successfully written!");
+            setText("");
+        }).catch((error) => {
+            alert("Error writing document: " + error);
+            setText("");
+        });
+    }    
+
 
     return (
         <ScrollView contentContainerStyle={{ alignItems: "center", paddingVertical: 20 }}>
             <LargeText>Our Cohort PMs:</LargeText>
 
             {/* JSX Components go here */}
+            <TextInput value={text} onChangeText={onChange} placeholder="Type username ..." marginTop={10}/>
+            <NormalText>Type: {text}</NormalText>
+
+            <Button onPress={() => addUsername(text)} marginVertical={10}>Add Username</Button>
+
+            <HorizontalRow>
+                <Filter checked={longName} onPress={() => {
+                    setLongName(!longName);
+                    setShortName(false);
+                }} text="Long Names"/>
+                <Filter checked={shortName} onPress={() => {
+                    setLongName(false);
+                    setShortName(!shortName);
+                }} text="Short Names"/>
+            </HorizontalRow>
+
+
+            {events.slice(0, 10).map((event, index) => (
+                <NormalText key={index}>{event}</NormalText>
+            ))}
 
             <View style={styles.row}>
                 <Button onPress={() => {
