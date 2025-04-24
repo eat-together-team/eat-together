1dee0edaaad9c82eac9167d3d15b9796d2a2d558
Matt Lau <mlau191@uw.edu>
2025-04-23 23:33:58 -0700

    Added Restaurant header and card component

---
+++ src/components/RestaurantCard.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {View, StyleSheet} from 'react-native';
+const RestaurantCard = () => {
+  return (
+    <View style = {{width:311,
+        height: 470,
+        backgroundColor: "#FFFFFF",
+        borderRadius: 40,
+        shadowOpacity: 0.25,
+        shadowOffset: {
+            width: 0,
+            height: 4,
+        },}}>
+    </View>
+  )
+}
+export default RestaurantCard
--- src/screens/RestaurantPicker/Restaurant.js
+++ src/screens/RestaurantPicker/Restaurant.js
@@ -1,19 +1,29 @@
 import React from 'react'
-import {Text, SafeAreaView} from "react-native";
+import {StyleSheet, View} from "react-native";
 import { Layout, TopNav} from "react-native-rapi-ui";
 import MediumText from "../../components/MediumText";
-import Ionicons from "react-native-vector-icons";
+import { Ionicons } from "@expo/vector-icons";
+import RestaurantCard from "../../components/RestaurantCard";
 const Restaurant = ({navigation}) => {
   return (
     <Layout>
-      {/* <TopNav
-        middleContent={<MediumText>Where To Eat</MediumText>}
+      <TopNav 
+        middleContent={<MediumText>Discover</MediumText>}
         leftContent={<Ionicons name="chevron-back" size={20} />}
-        leftAction={() => navigation.goBack()}
-        /> */}
-      <Text>This is test</Text>
+        onPress= {() => navigation.goBack()}
+      />
+      <View style = {styles.outerContainer}>
+        <RestaurantCard/>
+      </View>
     </Layout>
   )
 }
 
+const styles = StyleSheet.create({
+  outerContainer:{
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center',
+  }
+})
 export default Restaurant
