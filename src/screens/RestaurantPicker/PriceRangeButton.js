a82a6852bb1514f816013ee7d359e0410a3c19bf
Matt Lau <mlau191@uw.edu>
2025-04-24 16:29:33 -0700

    Added cuisine, dietary, and price range card components

---
+++ src/screens/RestaurantPicker/PriceRangeButton.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import {TouchableOpacity, Text, StyleSheet} from 'react-native';
+const PriceRangeButton = ({dollars, text}) => {
+  return (
+    <TouchableOpacity style = {styles.priceContainer}>
+      <Text>
+        <Text style = {{color: "#5DB075", fontSize: 15, fontWeight: 700}}>{dollars}</Text>
+        <Text style = {{color:"#A9A9A9", fontSize: 15, fontWeight: 700}}>{text}</Text>
+      </Text>
+    </TouchableOpacity>
+  )
+}
+const styles = StyleSheet.create({
+  priceContainer:{
+      display:'flex',
+      alignItems:'center',
+      justifyContent:'center',
+      width: 190,
+      height: 40,
+      backgroundColor:"#FFFFFF",
+      borderRadius:10,
+      shadowOpacity: 0.25,
+      shadowOffset: {
+          width: 0,
+          height: 4,
+      },
+  }
+})
+export default PriceRangeButton
--- src/screens/RestaurantPicker/Restaurant.js
+++ src/screens/RestaurantPicker/Restaurant.js
@@ -1,10 +1,15 @@
-import React from 'react'
-import {StyleSheet, View} from "react-native";
+import React, {useState} from 'react'
+import {StyleSheet, View, TextInput, Text} from "react-native";
 import { Layout, TopNav} from "react-native-rapi-ui";
 import MediumText from "../../components/MediumText";
 import { Ionicons } from "@expo/vector-icons";
-import RestaurantCard from "../../components/RestaurantCard";
+import CuisineCard from './CuisineCard';
+import DietaryPref from './DietaryPref';
+import CardCarousel from './CardCarousel';
+import PriceRangeCard from './PriceRangeCard';
+
 const Restaurant = ({navigation}) => {
+  const cards = [<CuisineCard/>, <DietaryPref/>, <PriceRangeCard/>]
   return (
     <Layout>
       <TopNav 
@@ -13,7 +18,7 @@ const Restaurant = ({navigation}) => {
         onPress= {() => navigation.goBack()}
       />
       <View style = {styles.outerContainer}>
-        <RestaurantCard/>
+        <CardCarousel cards = {cards}/>
       </View>
     </Layout>
   )
@@ -24,6 +29,12 @@ const styles = StyleSheet.create({
     flex:1,
     justifyContent:'center',
     alignItems:'center',
+  },
+  buttonContainer:{
+    display:'flex',
+    flexDirection:'row',
+    justifyContent:'center',
+    padding:20,
   }
 })
 export default Restaurant
