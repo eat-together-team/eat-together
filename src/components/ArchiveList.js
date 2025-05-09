09725c8306d602a714e663736bf73100e0fe752c
sixing-tao <jwwwt7777@gmail.com>
2025-05-09 14:34:43 -0700

    UI changes

---
+++ src/components/ArchiveList.js
@@ -0,0 +1,57 @@
+import React from "react";
+import { View, ScrollView, Image } from "react-native";
+import NormalText from "./NormalText";
+
+const ArchiveList = props => {
+    if (!props.archives || props.archives.length === 0) {
+        return (
+            <View style={{ padding: 18 }}>
+                <NormalText style={{ color: '#888', fontSize: 16 }}>
+                    You don't have any archives yet.
+                </NormalText>
+            </View>
+        );
+    }
+    return (
+        <View style={[{ flexDirection: "row" }, props.style]}>
+            <ScrollView horizontal={true} style={{ marginVertical: props.marginVertical !== undefined ? props.marginVertical : 0 }}
+                contentContainerStyle={{ flexGrow: 1, justifyContent: 'flex-start' }}>
+                {props.archives.map((archive, i) => (
+                    <View key={archive.id || i} style={{
+                        width: 140,
+                        height: 140,
+                        marginRight: 16,
+                        borderRadius: 16,
+                        overflow: 'hidden',
+                        backgroundColor: '#eee',
+                        position: 'relative',
+                        justifyContent: 'flex-end',
+                    }}>
+                        <Image
+                            source={archive.hasImage ? { uri: archive.image } : require("../../assets/foodBackground.png")}
+                            style={{ width: '100%', height: '100%', position: 'absolute', top: 0, left: 0 }}
+                            resizeMode="cover"
+                        />
+                        <View style={{
+                            width: '100%',
+                            padding: 10,
+                            backgroundColor: 'rgba(0,0,0,0.25)',
+                            position: 'absolute',
+                            bottom: 0,
+                            left: 0,
+                        }}>
+                            <NormalText style={{ fontWeight: 'bold', fontSize: 15, color: '#fff' }} numberOfLines={1}>
+                                {archive.name || 'Event Title'}
+                            </NormalText>
+                            <NormalText style={{ color: '#fff', fontSize: 13 }} numberOfLines={1}>
+                                {archive.startDate && archive.startDate.toDate ? archive.startDate.toDate().toLocaleDateString() : 'Date'}
+                            </NormalText>
+                        </View>
+                    </View>
+                ))}
+            </ScrollView>
+        </View>
+    );
+}
+
+export default ArchiveList; 
--- src/components/TagsList.js
+++ src/components/TagsList.js
@@ -4,8 +4,8 @@ import Tag from "./Tag";
 
 const TagsList = props => {
     return (
-        <View style={{ flexDirection: "row" }} onStartShouldSetResponder={() => props.remove ? true : false}>
-            <ScrollView horizontal={true} style={{ marginVertical: props.marginVertical ? props.marginVertical : 10 }}
+        <View style={[{ flexDirection: "row" }, props.style]} onStartShouldSetResponder={() => props.remove ? true : false}>
+            <ScrollView horizontal={true} style={{ marginVertical: props.marginVertical !== undefined ? props.marginVertical : 0 }}
                 contentContainerStyle={{ flexGrow: 1, justifyContent: props.left ? "flex-start" : "center" }}>
             {props.tags.map((tag, i) => <Tag text={tag.tag ? tag.tag : tag} key={tag.tag ? tag.tag : tag}
                 type={tag.type ? tag.type : null} remove={props.remove ? () => props.remove(tag, i) : false}/>)}
