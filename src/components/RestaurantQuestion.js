6aa2d09a259447d9a036e58c85ffe6cafa48fefd
Matt Lau <mlau191@uw.edu>
2025-05-10 14:48:17 -0700

    Fixed font size for start and dont worry card

--- src/components/RestaurantQuestion.js
+++ src/components/RestaurantQuestion.js
@@ -3,8 +3,14 @@ import {Text, StyleSheet} from 'react-native';
 
 const RestaurantQuestion = (props) => {
   return (
-    <Text style = {{fontFamily:'Inter', fontWeight:'700', fontSize:18, color:'#5DB075',
-     textAlign:'center'}}>
+    <Text style = {{
+      fontFamily:'Inter',
+      fontWeight:'700', 
+      fontSize: props.size? 
+      props.size : 18,
+      marginBottom: props.marginBottom ? props.marginBottom : 0,
+      color:'#5DB075',
+      textAlign:'center'}}>
         {props.text}
     </Text>
   )
